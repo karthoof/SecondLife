6071529084d3cca4707d718f1e8625fefb935d8c
Artem Kopytok <ar.bass@icloud.com>
2016-06-14 22:42:39 +0300

    Hello World /greeting убрал провайдед добавил шаблонизатор "лиф"

--- src/main/java/com/digdes/scrum/ScrumApplication.java
+++ src/main/java/com/digdes/scrum/ScrumApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScrumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScrumApplication.class, args);
+		SpringApplication.run (ScrumApplication.class, args);
 	}
 }
