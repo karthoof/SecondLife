7e1426ed642b3d4b6408753964965d0fc0ec47d8
Artem Kopytok <ar.bass@icloud.com>
2016-06-14 18:47:31 +0300

    Chepuha commit

--- just/src/Main.java
+++ just/src/Main.java
@@ -5,6 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+
+        String s = "Hello";
+
+        System.out.println(s);
+
     }
 
 }
